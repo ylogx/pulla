3d490e6b30b2c0f4dbc16bb7b5b2fc66eba9eecd
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-02-27 11:53:10 +0530

    Added argparse

--- pulla/main.py
+++ pulla/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+import argparse
 
 class Pulla:
     ''' Pulla main class
@@ -35,10 +36,27 @@ class Pulla:
 def main():
     ''' Main
     '''
-    curdir = os.path.abspath(os.curdir)
-    print('Current Directory is: ', curdir)
+    # Parse command line arguments
+    #usage = "%prog [-f credential_file]"
+    #parser = ArgumentParser(usage=usage)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", type=str, dest="folder",
+                        help="Update the repos in this folder")
+    parser.add_argument("-v", "--verbose", required=False, dest="verbosity",
+                        type=int, default=1, help="Verbosity")
+    #parser.add_argument('otherthings', nargs='*')
+    #args = parser.parse_args()
+    args, otherthings = parser.parse_known_args()
+    argc = len(otherthings)
+
+
+    directory = os.path.abspath(os.curdir)
+    if args.folder:
+        directory = args.folder
+
+    print('Directory is: ', directory)
     pulla = Pulla(verbosity=True, recursive=False)
-    pulla.pull_all(curdir)
+    pulla.pull_all(directory)
 
 if __name__ == '__main__':
     sys.exit(main())
