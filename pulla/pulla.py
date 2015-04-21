1871361a65f4847936da2176de8f989d8f89d8ce
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-04-22 02:20:06 +0530

    Extract logic to pull and to print output separatly

--- pulla/pulla.py
+++ pulla/pulla.py
@@ -14,22 +14,26 @@ class Pulla:
 
     def pull_all(self, folder):
         for (dirpath, dirnames, _) in os.walk(folder):
-            for dirname in dirnames:
-                if is_this_a_git_dir(dirname):
-                    if self.verbosity:
-                        print('----------------------')
-                    status = self.do_pull_in(dirname)
-                    status_msg = 'Fail'
-                    if status == 0:
-                        status_msg = 'Success'
-                    print('{0:<20} {1:<10}'.format(os.path.join(dirname), status_msg))
-                    if self.verbosity:
-                        print('----------------------')
+            for directory in dirnames:
+                if is_this_a_git_dir(directory):
+                    self.do_pull_in(directory)
             if not self.recursive:
                 break
         return None
 
     def do_pull_in(self, directory):
+        if self.verbosity:
+            print('----------------------')
+        status = self.perform_git_pull(directory)
+        status_msg = 'Fail'
+        if status == 0:
+            status_msg = 'Success'
+        print('{0:<20} {1:<10}'.format(os.path.join(directory), status_msg))
+        if self.verbosity:
+            print('----------------------')
+
+
+    def perform_git_pull(self, directory):
         can_use_c_flag = self.get_git_version() >= VERSION_WITH_C_FLAG_SUPPORT
         if can_use_c_flag:
             cmd = 'git -C ' + directory + ' pull'
