2ff118cd944913d6ae08beafbf96dda8e285e69f
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-04-28 03:22:06 +0530

    Bump version number to v0.0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ fhan.close()
 setup(
         name='Pulla',
         description='Pulla helps you pull content from all repos in any folder',
-        version='0.0.5',
+        version='0.0.6',
         packages=['pulla'],
         license='GPLv3+',
         author='Shubham Chaudhary',
