77a72e03d0b6e0706f02c987eaefa053a8bddc81
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-27 09:16:46 +0530

    Show dummy version number if distribution not found
    Mainly useful for running nosetests without installation

--- pulla/__init__.py
+++ pulla/__init__.py
@@ -1,6 +1,9 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
 
 from .pulla import Pulla
 from .logger import Logger
 
-__version__ = get_distribution('pulla').version
+try:
+    __version__ = get_distribution('pulla').version
+except DistributionNotFound:
+    __version__ = '0.0.0'
